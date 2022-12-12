2ea6cb355d426c415975e5f02f5dd2595f6da97a
barsu45 <barsaraniojha@gmail.com>
2022-12-12 17:38:53 +0530

    added

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ require(`dotenv`).config();
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin: "*"
+}))
+
 app.get("/", (req, res) => {
     res.send("Welcome to Homepage")
 })
